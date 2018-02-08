d99fd182a746132a79fde998c332ea7444313008
sumit.ranjan@in.pega.com <ranjs1@WNIPINW7.rpega.com>
2018-02-08 16:26:33 +0530

    Remove delay and add test suite

--- Test Suites/RC Test Suite.ts
+++ Test Suites/RC Test Suite.ts
@@ -1,20 +1,19 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
-   <description>All users can Login to Delivery Automation Portal</description>
-   <name>LoginTest</name>
+   <description></description>
+   <name>RC Test Suite</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-02-01T19:25:08</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <testSuiteGuid>82029a70-4f3c-406b-998b-589facd7e3fd</testSuiteGuid>
+   <testSuiteGuid>81fabfda-59db-4efd-93ce-69c93ca57de2</testSuiteGuid>
    <testCaseLink>
-      <guid>92e1f389-f6c1-4ac9-ac0b-1b5f898e04ae</guid>
+      <guid>c993f971-13d5-4553-a020-8cc2ec8f0390</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/LoginAsGsopsUser</testCaseId>
+      <testCaseId>Test Cases/Rate Card Automation Test Case</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
