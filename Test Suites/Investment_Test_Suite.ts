6ae64bb43527df568ebe497277e5c93a2d2cc6dc
ranjs1 <sumit.ranjan@in.pega.com>
2018-03-23 15:16:53 +0530

    Test suite created

---
+++ Test Suites/Investment_Test_Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Investment_Test_Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-23T15:15:21</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>bbfce91a-d6a4-4f40-b320-b86fb3002af4</testSuiteGuid>
+   <testCaseLink>
+      <guid>13c88a72-4a06-476f-99ea-d8bee3a7033e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Investment/NewInvestmentTestCases/Investment_Less_Than_100k</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Priage_Test_Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Priage_Test_Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>da3e36e7-312b-4f9f-93cb-020e7bb2b45e</testSuiteGuid>
+   <testCaseLink>
+      <guid>b26f3a5d-fddf-4366-a9fd-7ffbd4ce6485</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Priage/Create Priage</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
