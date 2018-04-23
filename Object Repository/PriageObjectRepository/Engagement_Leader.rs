ab7ea0dd591890008273b86308aef99239c41636
ranjs1 <sumit.ranjan@in.pega.com>
2018-04-23 16:51:03 +0530

    PRG Chnages

---
+++ Object Repository/PriageObjectRepository/Engagement_Leader.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Engagement_Leader</name>
+   <tag></tag>
+   <elementGuidId>8b28ab54-beff-4d95-b4cf-5b319164d9d9</elementGuidId>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>.//*[@id='EngagementLeaderID']</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ref_element</name>
+      <type>Main</type>
+      <value>Object Repository/Frames/frame1</value>
+   </webElementProperties>
+</WebElementEntity>
